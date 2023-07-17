76fde99cb54dca199f4a2e4558907b9394562ac0
hizmailovich <theannalios@gmail.com>
2023-07-17 11:34:56 +0300

    Resolved #7

---
+++ arithmetic/subtraction.test.js
@@ -0,0 +1,24 @@
+import {Subtraction} from "./subtraction.js";
+
+describe('Subtraction', () => {
+    test('0 - 0 = 0', () => {
+        expect(new Subtraction("0", "0").calculate())
+            .toBe("0");
+    });
+    test('1 - 2 = -1', () => {
+        expect(new Subtraction("1", "2").calculate())
+            .toBe("-1");
+    });
+    test('2222 - 100 = 2122', () => {
+        expect(new Subtraction("2222", "100").calculate())
+            .toBe("2122");
+    });
+    test('5000000 - 100000 = 4900000', () => {
+        expect(new Subtraction("5000000", "100000").calculate())
+            .toBe("4900000");
+    });
+    test('Wrong parameters', () => {
+        expect(() => new Subtraction("egrer", "858473").calculate())
+            .toThrow(TypeError);
+    });
+});
