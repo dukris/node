080cbbc863fce3f32e75eaa90c099918c7015a29
hizmailovich <theannalios@gmail.com>
2023-07-17 11:59:41 +0300

    Multiplication fixed

--- arithmetic/multiplication.js
+++ arithmetic/multiplication.js
@@ -44,7 +44,7 @@ export class Multiplication {
             answer.push(sum);
         }
         return answer.reduce((a, b) => new Addition(a, b).calculate(), "0")
-            .replace(/^0+(?=\d)/, '');;
+            .replace(/^0+(?=\d)/, '');
     }
 
 }
