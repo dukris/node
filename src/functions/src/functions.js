6cbb5523bd2c2ccbadb469195861332e8df50f29
hizmailovich <theannalios@gmail.com>
2023-07-28 16:10:41 +0300

    Function calculateDiscountedPrice added

---
+++ src/functions/src/functions.js
@@ -0,0 +1,14 @@
+/**
+ * Calculate discounted price.
+ *
+ * @param products Array of products
+ * @param percentage Discount percentage
+ * @returns {*[]} New array of products
+ */
+export const calculateDiscountedPrice = (products, percentage) => {
+    return JSON.parse(JSON.stringify(products))
+        .map(product => {
+            product.price = product.price - product.price * percentage / 100;
+            return product;
+        });
+}
