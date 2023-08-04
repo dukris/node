f345ecf2cfd1a8996eda3b2b3a27bd095a4b3222
hizmailovich <theannalios@gmail.com>
2023-08-04 13:22:39 +0300

    Chunk array function implemented

--- src/arrays/src/arrays.js
+++ src/arrays/src/arrays.js
@@ -11,3 +11,21 @@ export const customFilterUnique = (array, fun) => {
     }
     return array.filter((...args) => fun(...args));
 }
+
+/**
+ * Divide the array into smaller arrays, each containing elements of the specified chunk size.
+ *
+ * @param array Array
+ * @param size Chunk size
+ * @returns {*[]} Array of arrays
+ */
+export const chunkArray = (array, size) => {
+    if (typeof array != 'object' || typeof size != 'number' || size < 1) {
+        throw new TypeError("Input format is wrong!");
+    }
+    const arrays = [];
+    for (let i = 0; i < array.length; i += size) {
+        arrays.push(array.slice(i, i + size));
+    }
+    return arrays;
+}
