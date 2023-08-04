18acef0b274ef2c057747e56d08c11cecb3440da
hizmailovich <theannalios@gmail.com>
2023-08-04 13:52:22 +0300

    Tests updated

--- src/arrays/test/arrays.test.js
+++ src/arrays/test/arrays.test.js
@@ -1,4 +1,4 @@
-import {chunkArray, customFilterUnique} from "../src/arrays.js";
+import {chunkArray, customFilterUnique, getArrayIntersection, getArrayUnion} from "../src/arrays.js";
 
 describe("Advanced Array Filtering", () => {
     test("Filter array", () => {
@@ -66,4 +66,16 @@ describe("Array Chunking", () => {
         expect(() => chunkArray(array, 0)).toThrow(TypeError);
     });
 });
+
+describe("Array Intersection and Union", () => {
+    const first = [1, 2, 3, 4, 5, 6];
+    const second = [1, 2, 3, 8, 9];
+    test("Array Intersection", () => {
+        expect(getArrayIntersection(first, second)).toStrictEqual([1, 2, 3]);
+    });
+    test("Array Union", () => {
+        expect(getArrayUnion(first, second)).toStrictEqual([1, 2, 3, 4, 5, 6, 8, 9]);
+    });
+});
+
 
