0326bc4265c3f886aa0182006ec0431c6d04c49c
hizmailovich <theannalios@gmail.com>
2023-08-07 11:55:57 +0300

    Test implemented

--- src/objects/test/deep-cloning.test.js
+++ src/objects/test/deep-cloning.test.js
@@ -1,13 +1,41 @@
-import {deepCloneObject} from "../src/deep-cloning.js";
+import {deepClone, deepCloneObject} from "../src/deep-cloning.js";
 import {person} from "../src/prop-manipulation.js";
 
 describe("Object Deep Cloning", () => {
-    test("Deep cloning", () => {
+    test("Recursive deep cloning", () => {
         const clone = deepCloneObject(person);
         expect(clone).toStrictEqual(person);
         clone.firstName = "Jane";
         expect(clone.firstName).toBe("Jane");
         expect(person.firstName).toBe("John");
     });
+    test("Deep cloning", () => {
+        const obj = {
+            firstName: "John",
+            lastName: "Doe",
+            age: 30,
+            email: "john.doe@example.com",
+            addresses: [
+                {
+                    id: 1,
+                    value: {
+                        name: "Address 1"
+                    }
+                },
+                {
+                    id: 2,
+                    value: {
+                        name: "Address 2"
+                    }
+                }
+            ]
+        };
+        const clone = deepClone(obj);
+        expect(clone).toStrictEqual(obj);
+        clone.firstName = "Jane";
+        expect(clone.firstName).toBe("Jane");
+        expect(obj.firstName).toBe("John");
+    });
+
 });
 
