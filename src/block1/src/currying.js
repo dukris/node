d0019d2dc644f766bc4db1acae9207baa4996ae2
hizmailovich <theannalios@gmail.com>
2023-08-14 17:29:27 +0300

    Currying function updated

--- src/block1/src/currying.js
+++ src/block1/src/currying.js
@@ -2,14 +2,16 @@
  * Currying function.
  *
  * @param func Function to be curried
- * @param arity Number of arguments the original function takes
  * @returns {(function(...[*]): (*))|*} New curried function that waits for the remaining arguments
  */
-export const curry = (func, arity) => {
-    if (typeof func != 'function' || typeof arity != 'number') {
+export const curry = (func) => {
+    if (typeof func != 'function') {
         throw new TypeError("Input format is wrong!");
     }
     return function curried(...args1) {
+        if (args1.join(" ").match(/_/g)) {
+            args1 = args1.filter(element => element !== "_");
+        }
         if (args1.length >= func.length) {
             return func(...args1);
         }
--- src/block1/test/currying.test.js
+++ src/block1/test/currying.test.js
@@ -6,17 +6,16 @@ describe("Currying Function Implementation", () => {
     }
 
     test("Currying Function 1", () => {
-        const curriedMultiply = curry(multiply, 3);
-        const step1 = curriedMultiply(2);
-        const step2 = step1(3);
-        const result = step2(4);
-        expect(result).toBe(24);
+        const curriedMultiply = curry(multiply);
+        expect(curriedMultiply(2)(3)(4)).toBe(24);
     });
     test("Currying Function 2", () => {
-        const curriedMultiply = curry(multiply, 3);
-        const step1 = curriedMultiply(2);
-        const step2 = step1(3, 4);
-        expect(step2).toBe(24);
+        const curriedMultiply = curry(multiply);
+        expect(curriedMultiply(2)(3,4)).toBe(24);
+    });
+    test("Currying Function with _", () => {
+        const curriedMultiply = curry(multiply);
+        expect(curriedMultiply(2)("_", 3)("_")("_")(4)).toBe(24);
     });
 });
 
